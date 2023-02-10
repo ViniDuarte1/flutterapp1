cbe81ce1ca0a308f4fe5d31fe4868f514196e248
Vinicius Duarte <105808434+ViniDuarte1@users.noreply.github.com>
2023-02-10 11:12:23 -0300

    refatoração
    refatorado dificuldade

---
+++ difficulty.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Difficulty extends StatelessWidget {
+  final int difficultylevel;
+
+  const Difficulty({
+    required this.difficultylevel,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultylevel >= 1) ? Colors.blue : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultylevel >= 2) ? Colors.blue : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultylevel >= 3) ? Colors.blue : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultylevel >= 4) ? Colors.blue : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultylevel >= 5) ? Colors.blue : Colors.blue[100],
+        ),
+      ],
+    );
+  }
+}
