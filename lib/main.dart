902d7798aeeee0f8f005e22072b7afb138a67403
Vinicius Duarte <105808434+ViniDuarte1@users.noreply.github.com>
2023-02-10 10:48:03 -0300

    Condiçoes
    Adcicionado condiçoes e iniciado a refatoração

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacity = true;
 
   // This widget is the root of your application.
   @override
@@ -16,36 +23,54 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tarefas'),
-          leading: Container(),
-        ),
-        body: ListView(
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(top: 8),
-              child: Tasks(
-                'Estudar Flutter', 'http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png',
-              ),
+          appBar: AppBar(
+            title: const Text('Tarefas'),
+            leading: Container(),
+          ),
+          body: AnimatedOpacity(
+            opacity: opacity ? 1 : 0,
+            duration: Duration(seconds: 3),
+            child: ListView(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(top: 8),
+                  child: Tasks(
+                    'Estudar Flutter',
+                    'http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png',
+                    5,
+                  ),
+                ),
+                Tasks(
+                  'Andar de Bike',
+                  'http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png',
+                  4,
+                ),
+                Tasks(
+                  'Ler',
+                  'http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png',
+                  3,
+                ),
+                Tasks(
+                  'Meditar',
+                  'http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png',
+                  2,
+                ),
+                Tasks(
+                  'Jogar',
+                  'https://i0.wp.com/jornal.usp.br/wp-content/uploads/2022/06/202205601_jogos_streaming.jpg?w=1200&ssl=1',
+                  0,
+                )
+              ],
             ),
-            Tasks(
-              'Andar de Bike', 'https://www.bikemagazine.com.br/wp-content/uploads/2012/02/primeira-bike1.jpg',
-            ),
-            Tasks(
-              'Ler', 'https://editorialpaco.com.br/wp-content/uploads/2019/07/c8c74d9bca132ddb5e608bb5b212b386.jpg',
-            ),
-            Tasks(
-              'Meditar', 'https://img.freepik.com/vetores-gratis/mulher-relaxada-meditando_23-2148525630.jpg?w=826&t=st=1674743507~exp=1674744107~hmac=8caa127ed4bb91596e9ec74383bc85c9b8104eb981455217affb706e768f5d46',
-            ),
-            Tasks(
-              'Jogar', 'https://i0.wp.com/jornal.usp.br/wp-content/uploads/2022/06/202205601_jogos_streaming.jpg?w=1200&ssl=1',
-            )
-          ],
-        ),
-        /*       floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-        ), */
-      ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                opacity = !opacity;
+              });
+            },
+            child: Icon(Icons.remove_red_eye),
+          )),
     );
   }
 }
@@ -53,122 +78,183 @@ class MyApp extends StatelessWidget {
 class Tasks extends StatefulWidget {
   final String nome;
   final String foto;
-  const Tasks(this.nome, this.foto, {Key? key}) : super(key: key);
+  final int dificuldade;
+  const Tasks(this.nome, this.foto, this.dificuldade, {Key? key})
+      : super(key: key);
 
   @override
   State<Tasks> createState() => _TasksState();
 }
 
 class _TasksState extends State<Tasks> {
+  int level = 1;
+  double? a;
+
+  void levelUp() {
+    setState(() {
+      if (a != 1) {
+        level++;
+      }
+    });
+  }
+
   @override
-  int nivel = 0;
-  String teste = 'UP';
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                color: Colors.blue,
-              ),
-              height: 140,
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.blue,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: Colors.white,
-                  ),
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 72,
-                        height: 100,
+            height: 140,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                ),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
                         color: Colors.black26,
-                        child: Image.network(
-                            widget.foto,
-                            fit: BoxFit.cover),
                       ),
-                      Container(
-                        width: 200,
-                        child: Text(
-                          widget.nome,
-                          style: const TextStyle(
-                              fontSize: 24, overflow: TextOverflow.ellipsis),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Container(
-                          height: 52,
-                          width: 52,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              if (nivel < 10) {
-                                setState(() {
-                                  nivel++;
-                                });
-                              }
-                              if (nivel == 10) {
-                                setState(() {
-                                  teste = 'MAX';
-                                });
-                              }
-                            },
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Icon(Icons.arrow_drop_up),
-                                Text(
-                                  '$teste',
-                                  style: TextStyle(fontSize: 10),
-                                ),
-                              ],
-                            ),
+                      width: 72,
+                      height: 100,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(widget.foto, fit: BoxFit.cover)),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: const TextStyle(
+                                fontSize: 24, overflow: TextOverflow.ellipsis),
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+                        difficulty(widget: widget, widget: widget, widget: widget, widget: widget, widget: widget)
+                      ],
+                    ),
                     Padding(
-                      padding: const EdgeInsets.only(
-                        left: 8,
-                      ),
-                      child: Container(
-                        width: 200,
-                        child: LinearProgressIndicator(
-                          color: Colors.white,
-                          value: nivel / 10,
+                      padding: const EdgeInsets.all(16.0),
+                      child: SizedBox(
+                        height: 53,
+                        width: 53,
+                        child: ElevatedButton(
+                          onPressed: levelUp,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              const Icon(Icons.arrow_drop_up),
+                              Text(
+                                (a == 1) ? 'MAX' : 'UP',
+                                style: const TextStyle(fontSize: 9),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'Nivel: $nivel',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+                    child: SizedBox(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        value: (widget.dificuldade > 0)
+                            ? (a = (level / widget.dificuldade) / 10)
+                            : (a = level / 1),
                       ),
                     ),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Nivel: $level',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class difficulty extends StatelessWidget {
+  const difficulty({
+    Key? key,
+
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (widget.dificuldade >= 1)
+              ? Colors.blue
+              : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (widget.dificuldade >= 2)
+              ? Colors.blue
+              : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (widget.dificuldade >= 3)
+              ? Colors.blue
+              : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (widget.dificuldade >= 4)
+              ? Colors.blue
+              : Colors.blue[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (widget.dificuldade >= 5)
+              ? Colors.blue
+              : Colors.blue[100],
+        ),
+      ],
     );
   }
 }
