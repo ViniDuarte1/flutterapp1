80cfec512297e5c2b13b790c2494c67a251c1109
Vinicius Duarte <105808434+ViniDuarte1@users.noreply.github.com>
2023-02-13 11:42:27 -0300

    Atualização tela nova
    Adicionado formulario com nome, dificuldade e imagem para adicionar novas tasks

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -8,6 +8,9 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +25,83 @@ class _FormScreenState extends State<FormScreen> {
               color: Colors.black12,
               borderRadius: BorderRadius.circular(10),
               border: Border.all(color: Colors.black)),
-          child: Column(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nome',
+                    hintText: 'Digite seu nome',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: difficultyController,
+                  decoration: const InputDecoration(
+                    labelText: 'Dificuldade',
+                    hintText: 'Nível da dificuldade',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  onChanged: (text) {
+                    setState(() {});
+                  },
+                  controller: imageController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Imagem',
+                    hintText: 'url da imagem',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(width: 2, color: Colors.blue),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageController.text,
+                      errorBuilder: (BuildContext context, Object exception,
+                          StackTrace? stackTrace) {
+                        return Image.asset('assets/images/nophoto.png');
+                      },
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(nameController.text);
+                  print(int.parse(difficultyController.text));
+                  print(imageController.text);
+                },
+                child: Text('ADD TASK'),
+              ),
+            ],
+          ),
         ),
       ),
     );
